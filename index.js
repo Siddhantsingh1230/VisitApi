1331a3faf1db48e5fb9e8b3987c3670936b7ff37
Siddhantsingh1230 <Siddhantsingh9023134084@gmail.com>
2023-08-15 01:47:38 +0530

    added views and ejs engine

--- index.js
+++ index.js
@@ -2,10 +2,11 @@ const express = require("express");
 const app = express();
 // for parsing request
 app.use(express.json());
+//setting ejs engine
+app.set("view engine", "ejs");
 let countValue = 0;
 app.get("/", (req, res) => {
   countValue++;
-  const data = { author: "Siddhant Singh", clicks: countValue };
-  res.send(data);
+  res.render("home",{countValue});
 });
 app.listen(5000);
