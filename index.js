b219f0708b03d764074b40dabfe62f45c1bfb792
Siddhantsingh1230 <Siddhantsingh9023134084@gmail.com>
2023-08-15 02:03:34 +0530

    modified json response to html response

--- index.js
+++ index.js
@@ -2,11 +2,9 @@ const express = require("express");
 const app = express();
 // for parsing request
 app.use(express.json());
-//setting ejs engine
-app.set("view engine", "ejs");
 let countValue = 0;
 app.get("/", (req, res) => {
   countValue++;
-  res.render("home",{countValue});
+  res.send(`<h1>Count:${countValue}</h1>`);
 });
 app.listen(5000);
